a70ca63fb1633b995f9c5b80db5cadbbfb271498
Gavin <337507950@qq.com>
2026-02-08 13:13:18 +0800

    feat(workflow): add ASCII art banner to application startup

--- api-boot/src/main/java/com/api/Application.java
+++ api-boot/src/main/java/com/api/Application.java
@@ -14,5 +14,15 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+    System.out.println(
+        """
+#   # ##### #     #     #####     #   # ##### ####  #     ####    #
+#   # #     #     #     #   #     #   # #   # #   # #     #   #   #
+#   # #     #     #     #   #     #   # #   # #   # #     #   #   #
+##### ####  #     #     #   #     # # # #   # ####  #     #   #   #
+#   # #     #     #     #   #     # # # #   # # #   #     #   #   #
+#   # #     #     #     #   #     ## ## #   # #  #  #     #   #   #
+#   # ##### ##### ##### #####     #   # ##### #   # ##### ####    #
+""");
   }
 }
